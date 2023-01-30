97cab0f793eac05ec088e75eb8f70da69e278c17
Jeremie <jeremie.olivier.pro@gmail.com>
2023-01-30 19:47:14 +0100

    fix verifier flow

--- src/components/ClaimEcoID.tsx
+++ src/components/ClaimEcoID.tsx
@@ -7,11 +7,16 @@ import ButtonRegister from './ButtonRegister';
 import ButtonMintEcoID from './ButtonMintEcoID';
 import { Button } from '@mui/material';
 
+import { useAccount, useSigner, } from 'wagmi'
+
+
 function ClaimEcoID() {
 
 
     const globalServices = useContext(GlobalStateContext);
     const [state, send] = useActor(globalServices.stateService);
+    const { data: signer } = useSigner();
+
 
     return (
         <div style={{
@@ -27,7 +32,10 @@ function ClaimEcoID() {
 
             {state.matches({ "connected": { "claim eco id": { "attestation is loaded": "attestation is valid" } } }) ? <FormikForm></FormikForm> : ""}
             {state.matches({ "connected": { "claim eco id": "idle" } }) ? <UploadAttestation></UploadAttestation> : ""}
-            {state.matches({"connected":{"claim eco id":{"attestation is loaded":"attestation miss receiver signature"}}}) ? <Button variant="contained" onClick={()=> send("sign")}>Sign</Button> : ""}
+            {state.matches({"connected":{"claim eco id":{"attestation is loaded":"attestation miss receiver signature"}}}) ? <Button variant="contained" onClick={
+                //@ts-ignore
+                ()=> send("sign", signer)
+            }>Sign</Button> : ""}
 
             {state.matches({ "connected": { "claim eco id": { "attestation is loaded": "attestation ready to be registered" } } }) ? <ButtonRegister></ButtonRegister> : ""}
             {state.matches({ "connected": { "claim eco id": { "attestation is loaded": "Eco ID ready to be minted" } } }) ? <ButtonMintEcoID></ButtonMintEcoID> : ""}
