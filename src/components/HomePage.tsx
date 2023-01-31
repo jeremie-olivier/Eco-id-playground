0a1e19fef96e355a776a66ef5d0323b2e0ce6100
linahello <tropikalmoon@gmail.com>
2023-01-31 20:32:19 +0100

    formatting pages

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -4,6 +4,9 @@ import { useActor } from '@xstate/react';
 
 import Button from '@mui/material/Button';
 import Grid from '@mui/material/Unstable_Grid2';
+import CreateIcon from '@mui/icons-material/Create';
+import ComputerIcon from '@mui/icons-material/Computer';
+
 
 
 function HomePage() {
@@ -13,10 +16,10 @@ function HomePage() {
   return (
     <Grid container display="flex" justifyContent="center" alignItems="center" flexDirection="column" spacing={2}>
       <Grid>
-        <Button variant="contained" onClick={() => send('create')}>Create attestation</Button>
+        <Button variant="contained" color="success" endIcon={<CreateIcon/>} onClick={() => send('create')}>Create attestation</Button>
       </Grid>
       <Grid>
-        <Button variant="contained" onClick={() => send('claim')}>Claim attestation</Button>
+        <Button variant="contained" color="success" endIcon={<ComputerIcon/>} onClick={() => send('claim')}>Claim attestation</Button>
       </Grid>
     </Grid>
   );
--- src/components/landingpage/PageContainer.tsx
+++ src/components/landingpage/PageContainer.tsx
@@ -6,6 +6,8 @@ import HomePage from '../HomePage';
 import FormikForm from '../FormikForm';
 import ClaimEcoID from '../ClaimEcoID';
 import { Container } from '@mui/material';
+import Box from '@mui/material/Box';
+
 
 function PageContainer() {
 
@@ -14,13 +16,24 @@ function PageContainer() {
   const [state, send] = useActor(globalServices.stateService);
 
   return (
+
+    
     <Container sx={{ m: 2 }}>
+      <Box
+          sx={{
+            marginTop: 8,
+            margin: 8,
+            display: 'flex',
+            p: 2, border: '1px dashed green',           flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
 
       {state.matches('idle') ? <BlockInvitationToConnect></BlockInvitationToConnect> : ""}
       {state.matches({ "connected": "home page" }) ? <HomePage></HomePage> : ""}
       {state.matches({"connected":{"create attestation":"form is valid"}} ) ? <FormikForm></FormikForm> : ""}
       {state.matches({ "connected": "claim eco id" }) ? <ClaimEcoID></ClaimEcoID> : ""}
-
+      </Box>
     </Container>
   );
 };
