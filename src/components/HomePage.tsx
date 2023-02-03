170510224763a903872f33eb55b7e3df81be622f
linahello <tropikalmoon@gmail.com>
2023-02-03 16:36:43 +0100

    tooltip hmtl on the claim button from home page

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -10,8 +10,9 @@ import ComputerIcon from '@mui/icons-material/Computer';
 import Stack from '@mui/material/Stack';
 import Snackbar from '@mui/material/Snackbar';
 import MuiAlert, { AlertProps } from '@mui/material/Alert';
-import Tooltip from '@mui/material/Tooltip';
-
+import { styled } from '@mui/material/styles';
+import Tooltip, { TooltipProps, tooltipClasses } from '@mui/material/Tooltip';
+import Typography from '@mui/material/Typography';
 
 const Alert = React.forwardRef<HTMLDivElement, AlertProps>(function Alert(
   props,
@@ -19,6 +20,18 @@ const Alert = React.forwardRef<HTMLDivElement, AlertProps>(function Alert(
 ) {
   return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
 });
+
+const HtmlTooltip = styled(({ className, ...props }: TooltipProps) => (
+  <Tooltip {...props} classes={{ popper: className }} />
+))(({ theme }) => ({
+  [`& .${tooltipClasses.tooltip}`]: {
+    backgroundColor: '#f5f5f9',
+    color: 'rgba(0, 0, 0, 0.87)',
+    maxWidth: 220,
+    fontSize: theme.typography.pxToRem(12),
+    border: '1px solid #dadde9',
+  },
+}));
 
 function HomePage() {
   const globalServices = useContext(GlobalStateContext);
@@ -72,9 +85,19 @@ function HomePage() {
         </Grid>
 
         <Grid>
-          <Tooltip title="Upload and sign an already existing one" placement="right">
+
+          <HtmlTooltip
+        title={
+          <React.Fragment>
+            <Typography color="inherit">Tooltip with HTML</Typography>
+            <em>{"And here's"}</em> <b>{'some'}</b> <u>{'amazing content'}</u>.{' '}
+            {"Upload and sign an already existing one"}
+          </React.Fragment>
+        }
+      >
             <Button variant="contained" color="success" endIcon={<ComputerIcon/>} onClick={() => send('claim')}>Claim attestation</Button>
-          </Tooltip>
+            </HtmlTooltip>
+
         </Grid>
 
       </Grid>
