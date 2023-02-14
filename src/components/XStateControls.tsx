53136205a05bd86df4e1001aad721c1c7d4161bb
Jeremie <jeremie.olivier.pro@gmail.com>
2023-02-14 18:33:31 +0100

    add invitation to test state machine vizualizer

--- src/components/XStateControls.tsx
+++ src/components/XStateControls.tsx
@@ -30,16 +30,16 @@ function XStateControls() {
         bottom: 0,
         position: "fixed"
       }}>
-            
+
         <Typography gutterBottom variant="body2">
-          <LightbulbIcon/>How to use the app : Start by connecting your wallet. Then follow the tips !
+          <LightbulbIcon />If you want to better understand the logic behind this Dapp, click pop-up blocked <img src="//lh3.googleusercontent.com/eySL1iWYR66hTR7K0dPzOfUo-a7NXTNg3XbfsMHiCg6B4y6PeYOELaFsDsBrvT4wrA=h36" height="18" alt="Pop-up blocked" data-mime-type="image/png" data-alt-src="//lh3.googleusercontent.com/eySL1iWYR66hTR7K0dPzOfUo-a7NXTNg3XbfsMHiCg6B4y6PeYOELaFsDsBrvT4wrA"></img> in the address bar, to play with the Interactive State Machine
         </Typography>
         <Divider variant="middle" color="primary" />
-          <Grid container alignItems="center">
-            <Typography color="grey" variant="body2"> 
+        <Grid container alignItems="center">
+          <Typography color="grey" variant="body2">
             State machine current state : <span>{JSON.stringify(state.value)}</span>
-            </Typography>
-          </Grid>
+          </Typography>
+        </Grid>
       </Container>
     </ThemeProvider>
   )
